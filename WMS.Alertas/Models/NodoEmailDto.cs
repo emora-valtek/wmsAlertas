5cfbff55c633b26cdf34edf99cd4dab23e890dbc
emora-valtek <emora@valtek.cl>
2026-06-17 17:56:22 -0400

    cambios

--- WMS.Alertas/Models/NodoEmailDto.cs
+++ WMS.Alertas/Models/NodoEmailDto.cs
@@ -4,7 +4,7 @@
     {
         public string Email { get; set; }
         public string EmailContrasena { get; set; }
-        public string Host { get; set; }
+        public string Host { get; set; } 
         public int Puerto { get; set; }
         public bool UsaSSL { get; set; }
         public string Alias { get; set; }
